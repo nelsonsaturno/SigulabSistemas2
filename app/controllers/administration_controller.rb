68fd215c0e24d3c19759bbba0f96f57829481891
Manuel Pacheco <manuelalejandropm@gmail.com>
2014-11-04 15:01:50 -0430

    Divide el administration layout en diferentes archivos

--- app/controllers/administration_controller.rb
+++ app/controllers/administration_controller.rb
@@ -1,5 +1,15 @@
 class AdministrationController < ApplicationController
-
+  layout 'administration'
+  
+  before_action :set_subsystem 
+  
   def index
   end
+  
+  private
+  
+  def set_subsystem
+    @subsystem = 'administracion'
+  end
+  
 end
