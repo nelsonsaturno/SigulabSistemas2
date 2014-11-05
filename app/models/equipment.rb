2c624996f58617ee9817bf03ac607b94faa13c36
Basuco <alejandro.g.206@gmail.com>
2014-11-05 15:50:11 -0430

    Cambiando mensajes de error

--- app/models/equipment.rb
+++ app/models/equipment.rb
@@ -1,7 +1,7 @@
 class Equipment < ActiveRecord::Base
-	validates :name, presence: true
-	validates :serial, presence: true
-	validates :national_good, presence: true, uniqueness: true
+	validates :name, presence: true, :presence => {:message => "no puede ser blanco"}
+	validates :serial, presence: true, :presence => {:message => "no puede ser blanco"}
+	validates :national_good, :presence => {:message => "no puede ser blanco"}, uniqueness: true
 	validates :location, presence: true
 	validates :responsible, presence: true
 	serialize :use, JSON
