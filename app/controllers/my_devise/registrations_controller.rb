1b70177eb939ad456d7f723118b7cd69383ebb0f
victorjsm <juniusaga@gmail.com>
2014-11-06 09:10:00 -0430

    Se corrigio error en editar y en crear usuarios.

--- app/controllers/my_devise/registrations_controller.rb
+++ app/controllers/my_devise/registrations_controller.rb
@@ -1,25 +1,14 @@
 # app/controllers/my_devise/registrations_controller.rb
 class MyDevise::RegistrationsController < Devise::RegistrationsController
-	def new
-		@user = User.new
-	end
-	def create
-		@user = User.new(user_params)
-		respond_to do |format|
-			if @user.save
-				format.html { redirect_to root_path, notice: 'Usuario creado exitosamente!.' }
-				format.json { render action: 'Mostrar', status: :created, location: @user}
-			else
-				format.html { render action: 'Nuevo' }
-				format.json { render json: @user.errors, status: :unprocessable_entity }
-			end
-		end
-	end
+
 	def update
 		super
 	end
 	private
-	def user_params
-		params.require(:user).permit(:username, :email, :password, :administrator, :technician, :labBoss)
+	def sign_up_params
+		params.require(:user).permit(:username, :email, :password, :password_confirmation, :administrator, :technician, :labBoss)
+	end
+	def account_update_params
+		params.require(:user).permit(:username, :email, :password, :password_confirmation, :current_password, :administrator, :technician, :labBoss)
 	end
 end
