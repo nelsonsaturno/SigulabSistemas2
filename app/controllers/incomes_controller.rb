d69644b881d519063d94c742a5dbfc18de6a662a
Manuel Pacheco <manuelalejandropm@gmail.com>
2014-11-05 23:44:35 -0430

    Agrega las vistas index y show de Incomes

--- app/controllers/incomes_controller.rb
+++ app/controllers/incomes_controller.rb
@@ -2,6 +2,11 @@ class IncomesController < ApplicationController
 
   def index
     @incomes = Income.all
+    @sum = @incomes.sum(:amount)
   end
 
+  def show
+    @income = Income.find(params[:id])
+  end
+  
 end
