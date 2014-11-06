58b3ae2acc5f45c019fb8b13c617becf35a59131
Cristian Medina <cmedina270793@hotmail.com>
2014-11-06 13:08:19 -0430

    Se agregaron las validaciones para los datos en income y commitment

--- app/models/commitment.rb
+++ app/models/commitment.rb
@@ -4,4 +4,22 @@ class Commitment < ActiveRecord::Base
   
   has_many :executions
   belongs_to :lab
+
+  validates :amount, :precense => true,
+					 :numericality => true,
+					 :greater_than => 0
+
+  validates :code,	:precense => true
+
+  validates :lab_id, :presence => true,
+					 :numericality => true,
+					 :greater_than => 0,
+					 :less_than => 9
+
+   validates :description :presence => true,
+						 :length => {:maximum => 512}
+
+  validates :recipient :presence => true,
+					   :length => {:maxumum => 512} #!No se sabe si es obligatorio
+
 end
--- app/models/income.rb
+++ app/models/income.rb
@@ -1,4 +1,28 @@
 class Income < ActiveRecord::Base
   enum origin: [:ordinary, :extraordinary, :projects, :reformulation]
   belongs_to :lab
+
+  validates :amount, :precense => true,
+					 :numericality => true,
+					 :greater_than => 0
+
+  validates :organism, :precense => true,
+					   :length {:maximum => 512}
+
+  validates :lab_id, :presence => true,
+					 :numericality => true,
+					 :greater_than => 0,
+					 :less_than => 9
+
+  validates :description :presence => true,
+						 :length => {:maximum => 512}
+
+  validates :financing :presence => true,
+					   :length => {:maxumum => 512} #!No se sabe si es obligatorio
+
+  validates :document :presence => true,
+					  :length => {:maxumum => 512} #!No se sabe si es obligatorio
+
+  validates :date :format => /(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\d\d/
+
 end
