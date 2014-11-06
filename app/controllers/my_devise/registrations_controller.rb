250ef7dbbeabe79e61bdadc4d113198bcecab274
steeefyyy <stefanybotero@hotmail.com>
2014-11-05 23:22:10 -0430

    acomode un poco de cosas a espa;ol e idente otro poco de cosas de la interfaz yay!

--- app/controllers/my_devise/registrations_controller.rb
+++ app/controllers/my_devise/registrations_controller.rb
@@ -7,10 +7,10 @@ class MyDevise::RegistrationsController < Devise::RegistrationsController
 		@user = User.new(user_params)
 		respond_to do |format|
 			if @user.save
-				format.html { redirect_to root_path, notice: 'User was successfully created.' }
-				format.json { render action: 'show', status: :created, location: @user}
+				format.html { redirect_to root_path, notice: 'Usuario creado exitosamente!.' }
+				format.json { render action: 'Mostrar', status: :created, location: @user}
 			else
-				format.html { render action: 'new' }
+				format.html { render action: 'Nuevo' }
 				format.json { render json: @user.errors, status: :unprocessable_entity }
 			end
 		end
