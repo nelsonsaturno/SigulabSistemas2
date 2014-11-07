5fd0ed93156d45d14aaba05175c47e2e5828f173
Manuel Pacheco <manuelalejandropm@gmail.com>
2014-11-06 23:37:45 -0430

    Finaliza las acciones index show new y create de Income

--- app/models/income.rb
+++ app/models/income.rb
@@ -2,27 +2,15 @@ class Income < ActiveRecord::Base
   enum origin: [:ordinary, :extraordinary, :projects, :reformulation]
   belongs_to :lab
 
-  validates :amount, :precense => true,
-					 :numericality => true,
-					 :greater_than => 0
+  validates :lab, :presence => true
+  validates :amount, :presence => true, numericality: { greater_than: 0 }
+  validates :description, presence: true, length: {maximum: 512}
+  validates :origin, presence: true
+  validates :date, presence: true
+  validates_datetime :date
 
-  validates :organism, :precense => true,
-					   :length {:maximum => 512}
-
-  validates :lab_id, :presence => true,
-					 :numericality => true,
-					 :greater_than => 0,
-					 :less_than => 9
-
-  validates :description :presence => true,
-						 :length => {:maximum => 512}
-
-  validates :financing :presence => true,
-					   :length => {:maxumum => 512} #!No se sabe si es obligatorio
-
-  validates :document :presence => true,
-					  :length => {:maxumum => 512} #!No se sabe si es obligatorio
-
-  validates :date :format => /(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\d\d/
+  validates :organism, length: {maximum: 512}
+  validates :financing, length: {maximum: 512} #!No se sabe si es obligatorio
+  validates :document, length: {maximum: 512} #!No se sabe si es obligatorio
 
 end
