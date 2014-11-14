bb9e13a8b3bcf7c517787c54dfd3fae68e96d5a5
Carlo Polisano S <carlo.polisano@gmail.com>
2014-11-14 13:53:56 -0430

    pruebas 20% a compras

--- test/models/item_test.rb
+++ test/models/item_test.rb
@@ -4,4 +4,44 @@ class ItemTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+# 	fixtures :all
+	test "todosLosCamposVacios" do
+		item=Item.new
+		assert_not item.save
+	end
+	
+	test "todosLosCamposLlenos" do
+		item=items(:one)
+		assert item.save
+	end
+	
+	test "faltaNombre" do
+		item=items(:two)
+		assert_not item.save
+	end
+	
+	test "faltaTipo" do
+		item=items(:three)
+		assert_not item.save
+	end
+	
+	test "faltaDescripcion" do
+		item=items(:four)
+		assert_not item.save
+	end
+	
+	test "faltaDimeniones" do
+		item=items(:five)
+		assert item.save
+	end
+	
+	test "faltaCantidad" do
+		item=items(:six)
+		assert_not item.save
+	end
+	
+	test "faltaUnidad" do
+		item=items(:seven)
+		assert_not item.save
+	end
 end
